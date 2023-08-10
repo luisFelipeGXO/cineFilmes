a893352e3c3f5735acb2c5bed50485ed6cecdca4
luisFelipeGXO <gluisfelipe934@gmail.com>
2023-08-09 21:49:12 -0300

    commit inicial

---
+++ src/test/java/com/example/cineFilmes/CineFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cineFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
